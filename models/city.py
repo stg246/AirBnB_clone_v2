fddb2b697c4f645ed80a2613233e798edefc9841
Hess-y <113606328+Hess-y@users.noreply.github.com>
2023-04-17 23:43:59 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import relationship
 
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     state_id = Column(String(60), Foreignkey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
