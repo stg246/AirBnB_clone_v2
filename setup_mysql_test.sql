4d71eb22c963e1d54bc3171b30d942703a9431d3
Hess-y <113606328+Hess-y@users.noreply.github.com>
2023-04-18 14:10:23 +0300

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,14 @@
---A script that prepares a MySQL server for the project
+-- Create hbnb_test_db database if not exists
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create hbnb_test user with password hbnb_test_pwd if not exists
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant all privileges on hbnb_test_db to hbnb_test user
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant SELECT privilege on performance_schema to hbnb_test user
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Flush privileges to apply the changes
 FLUSH PRIVILEGES;
