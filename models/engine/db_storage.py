87bcc7c54904ee4a6d451cfbad81f44a6606d1ce
Hess-y <hessy683@gmail.com>
2023-04-18 02:36:02 -0700

    task update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -10,55 +10,64 @@ from models.amenity import Amenity
 from models.review import Review
 import os import getenv
 
+
 class DBStorage:
-      __engine = None
-      __session = None
-      
-      def __init__(self):
-        """ init method """
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
-            os.getenv('HBNB_MYSQL_USER'),
-            os.getenv('HBNB_MYSQL_PWD'),
-            os.getenv('HBNB_MYSQL_HOST'),
-            os.getenv('HBNB_MYSQL_DB')), pool_pre_ping=True)
-        
-         if os.getenv('HBNB_ENV') == 'test':
-            Base.metadata.drop_all(self.__engine)
-      
-      def all(self, cls=None):
-        """ all method """
-        dict_objs = {}
-        for clss in classes:
-            if cls is None or cls is classes[clss] or cls is clss:
-                objs = self.__session.query(classes[clss]).all()
-                for obj in objs:
-                    key = obj.__class__.__name__ + '.' + obj.id
-                    dict_objs[key] = obj
+    __engine = None
+    __session = None
+
+
+def __init__(self):
+    """ init method """
+    self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+        os.getenv('HBNB_MYSQL_USER'),
+        os.getenv('HBNB_MYSQL_PWD'),
+        os.getenv('HBNB_MYSQL_HOST'),
+        os.getenv('HBNB_MYSQL_DB')), pool_pre_ping=True)
+
+
+if os.getenv('HBNB_ENV') == 'test':
+    Base.metadata.drop_all(self.__engine)
+
+
+def all(self, cls=None):
+    """ all method """
+    dict_objs = {}
+    for clss in classes:
+        if cls is None or cls is classes[clss] or cls is clss:
+            objs = self.__session.query(classes[clss]).all()
+    for obj in objs:
+        key = obj.__class__.__name__ + '.' + obj.id
+        dict_objs[key] = obj
         return (dict_objs)
 
-      def new(self, obj):
-        """ new method """
-        self.__session.add(obj)
 
-      def save(self):
-        """ save method """
-        self.__session.commit()
+def new(self, obj):
+    """ new method """
+    self.__session.add(obj)
 
-      def delete(self, obj=None):
-        """ delete method """
-        if obj:
-            self.__session.delete(obj)
-            self.save()
 
-      def reload(self):
-        """ reload method """
-        Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine,
-                                       expire_on_commit=False)
-        Session = scoped_session(session_factory)
-        self.__session = Session()
+def save(self):
+    """ save method """
+    self.__session.commit()
 
-      def close(self):
-        """ close method """
-        if self.__session:
-            self.__session.close()
+
+def delete(self, obj=None):
+    """ delete method """
+    if obj:
+        self.__session.delete(obj)
+        self.save()
+
+
+def reload(self):
+    """ reload method """
+    Base.metadata.create_all(self.__engine)
+    session_factory = sessionmaker(bind=self.__engine,
+                                   expire_on_commit=False)
+    Session = scoped_session(session_factory)
+    self.__session = Session()
+
+
+def close(self):
+    """ close method """
+    if self.__session:
+        self.__session.close()
