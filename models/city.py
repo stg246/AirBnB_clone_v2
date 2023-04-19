5daed1ba505c2191278005df72f2a8d8897b5735
Hess-y <113606328+Hess-y@users.noreply.github.com>
2023-04-19 23:47:18 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -6,7 +6,15 @@ from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+   """Represents a city for a MySQL database.
+    
+    Inherits from SQLAlchemy Base and links to the MySQL table cities.
+    
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store Cities.
+        name (sqlalchemy String): The name of the City.
+        state_id (sqlalchemy String): The state id of the City.
+    """
     state_id = Column(String(60), Foreignkey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
     __tablename__ = 'cities'
