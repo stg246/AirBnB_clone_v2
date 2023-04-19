cb87c3450810feb83b2499d3d6bf752ea37f2189
Hess-y <113606328+Hess-y@users.noreply.github.com>
2023-04-19 23:31:09 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -7,13 +7,17 @@ import models
 
 
 class Review(BaseModel, Base):
-    """ Review classto store review information """
-    if models.is_type == 'db':
-        __tablename__ = 'reviews'
-        text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
+    """ Review classto store review information.
+    
+    Inherits from SQLAlchemy Base and links to the MySQL table reviews.
+    
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store Reviews.
+        text (sqlalchemy String): The review description.
+        place_id (sqlalchemy String): The review's place id.
+        user_id (sqlalchemy String): The review's user id.
+    """
+    __tablename__ = "reviews"
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
